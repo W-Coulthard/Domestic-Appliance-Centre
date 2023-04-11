eba311210977972f57abe42e643a00e4dd04f68a
W-Coulthard <96620785+W-Coulthard@users.noreply.github.com>
2023-04-11 12:41:22 +0100

    update: api key

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 1.1380160321422261, lng: 52.06683953846858 },
     zoom: 8,
   });
 }
